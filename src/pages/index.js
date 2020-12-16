d345a59b9a88943b82a75d3932e1722aaf64dfb1
alesservin <alejandro.sebastian.servin@gmail.com>
2020-12-16 15:03:48 -0300

    Add Collaborators sections with images, add custom css and delete old image

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,18 @@ import pic03 from '../assets/images/pic03.jpg'
 import pic04 from '../assets/images/pic04.jpg'
 import pic05 from '../assets/images/pic05.jpg'
 import pic06 from '../assets/images/pic06.jpg'
+import reaccionLogo from '../assets/newimages/reaccion-logo.png'
+import codiumLogo from '../assets/newimages/codium-logo.png'
+import cdsLogo from '../assets/newimages/cds-logo.png'
+import ideaLogo from '../assets/newimages/idea-logo.png'
+import girolabsLogo from '../assets/newimages/girolabs-logo.png'
+import civilabLogo from '../assets/newimages/civilab-logo.png'
+import aigaLogo from '../assets/newimages/aiga-logo.png'
+import paraguayoiteLogo from '../assets/newimages/paraguayoite-logo.png'
+import techoLogo from '../assets/newimages/techo-logo.png'
+import ptiLogo from '../assets/newimages/pti-logo.png'
+import nedLogo from '../assets/newimages/ned-logo.png'
+import '../assets/css/custom.css'
 
 class HomeIndex extends React.Component {
     render() {
@@ -34,7 +46,7 @@ class HomeIndex extends React.Component {
                                 <p>Podrás acceder a nuestros conjuntos de datos
                                 de uso abierto para todos</p>
                             </header>
-                            <a href="https://portal.datos.org.py/dataset" className="link primary"> </a>
+                            <a href="https://portal.datos.org.py/" className="link primary"> </a>
                         </article>
                         <article style={{backgroundImage: `url(${pic02})`}}>
                             <header className="major">
@@ -104,6 +116,64 @@ class HomeIndex extends React.Component {
                                 <li><a href="https://portal.datos.org.py/" className="button next">Comenzar</a></li>
                             </ul>
                         </div>
+                    </section>
+                    <section id="three" className="collaborators">
+                      <div className="community">
+                        <h2>Parte de la comunidad:</h2>
+                        <a href="https://www.reaccion.org.py" target="_blank"
+                          rel="noreferrer">
+                          <img src={reaccionLogo} alt="reaccion" />
+                        </a>
+                        <a href="http://codium.com.py" target="_blank"
+                          rel="noreferrer" >
+                          <img src={codiumLogo} alt="codium" />
+                        </a>
+                        <a href="https://cds.com.py/" target="_blank"
+                          rel="noreferrer" >
+                          <img src={cdsLogo} alt="cds" />
+                        </a>
+                        <a href="https://www.idea.org.py/" target="_blank"
+                          rel="noreferrer">
+                          <img src={ideaLogo} alt="idea"
+                            style={{height: '90px'}}/>
+                        </a>
+                        <a href="https://girolabs.com/" target="_blank"
+                          rel="noreferrer">
+                          <img src={girolabsLogo} alt="girolabs"
+                            style={{height:'35px'}}/>
+                        </a>
+                        <a href="https://civilab.org.py/" target="_blank"
+                          rel="noreferrer">
+                          <img src={civilabLogo} alt="civilab"
+                            style={{height:'80px'}}/>
+                        </a>
+                        <a href="https://www.reaccion.org.py/#cbp=portfolio/aiga"
+                         target="_blank" rel="noreferrer">
+                          <img src={aigaLogo} alt="aiga"
+                            style={{height:'120px'}}/>
+                        </a>
+                        <a href="https://www.reaccion.org.py/#cbp=portfolio/paraguayoite"
+                          target="_blank" rel="noreferrer">
+                          <img src={paraguayoiteLogo} alt="paraguayoite"
+                            style={{height:'80px'}}/>
+                        </a>
+                        <a href="https://www.techo.org.py/" target="_blank"
+                          rel="noreferrer">
+                          <img src={techoLogo} alt="techo"
+                            style={{height:'65px'}}/>
+                        </a>
+                      </div>
+                      <div className="supporters">
+                        <h2>Con apoyo de:</h2>
+                        <a href="https://www.pti.org.py/" target="_blank"
+                          rel="noreferrer">
+                          <img src={ptiLogo} alt="pti"/>
+                        </a>
+                        <a href="https://www.ned.org/" target="_blank"
+                          rel="noreferrer">
+                          <img src={nedLogo} alt="ned"/>
+                        </a>
+                      </div>
                     </section>
                 </div>
 
