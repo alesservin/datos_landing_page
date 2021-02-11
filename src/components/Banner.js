cc512ad5c9569bc90829244f1b7c6d78a5642b22
alesservin <alejandro.sebastian.servin@gmail.com>
2021-02-11 13:44:30 -0300

    Panteon's image is first image of banner and show Puente de la Amistad on CDE photo

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 import Carousel from 'react-bootstrap/Carousel'
-import bg1 from '../assets/images/bg1.jpg'
-import bg2 from '../assets/images/bg2.jpg'
-import bg3 from '../assets/images/bg3.jpg'
+import bg1 from '../assets/images/bg3.jpg'
+import bg2 from '../assets/images/bg1.jpg'
+import bg3 from '../assets/images/bg2.jpg'
 import bg4 from '../assets/images/bg4.jpg'
 import bg5 from '../assets/images/bg5.jpg'
 
